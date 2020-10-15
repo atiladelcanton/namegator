4d1c34b854fbc86e695017533ff56990ed5a78b8
Atila Rampazo <atilarampazo@gmail.com>
2020-10-15 00:14:05 -0300

    versionamento de acompanhamento

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="slogan">
+		<div class="text-center">
+			<h1>NanoGator <span class="fa fa-check"></span></h1>
+			<br/>
+			<h6 class="text-secondary">Gerador de nomes</h6>
+		</div>
+		<div id="main">
+				<DomainList></DomainList>
+		</div>
+	</div>
+</template>
+
+<script>
+import  "bootstrap/dist/css/bootstrap.css";
+import	DomainList from "./components/DomainList";
+export default {
+	name: "App",
+	components:{
+		DomainList
+	}
+};
+</script>
+
+<style>
+#slogan{
+	margin-top: 30px;
+	margin-bottom: 30px;
+}
+#main{
+	background-color: #F1F1F1;
+	padding-top: 30px;
+	padding-bottom: 20px;
+}
+</style>
