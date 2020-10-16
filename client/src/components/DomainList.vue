af740007cd1f6767a2f7f134f37970b8a3c93827
Atila Rampazo <atilarampazo@gmail.com>
2020-10-15 21:32:34 -0300

    feat:Adicionado VUEX e refatorado

--- client/src/components/DomainList.vue
+++ client/src/components/DomainList.vue
@@ -47,7 +47,7 @@
 
 <script>
 import  "bootstrap/dist/css/bootstrap.css";
-import axios from "axios/dist/axios";
+import {mapState, mapActions} from "vuex";
 import	AppItemList from "./AppItemList";
 export default {
 	name: "DomainList",
@@ -55,107 +55,10 @@ export default {
 		AppItemList
 	},
 	data: function(){
-		return {
-			items:	{
-				prefix:	[],
-				sufix:	[],
-			},
-			domains:[]
-		};
+		return {	};
 	},
 	methods:{
-		addItem(item){
-			
-			axios({
-				url:"http://localhost:4000",
-				method: "post",
-				data:{
-					query:`
-						mutation ($item: ItemInput) {
-							newItem: saveItem(item:  $item) {
-								id,
-								type,
-								description
-							}
-						}
-					`,
-					variables:{
-						item
-					}
-				}
-			})
-				.then( response =>{
-					const query	=	response.data;
-					const	newItem	=	query.data.newItem;
-					this.items.[item.type].push(newItem);
-					this.generateDomains();
-				});
-		},
-		deleteItem(item){
-			axios({
-				url:"http://localhost:4000",
-				method: "post",
-				data:{
-					query: `
-						mutation ($id: Int) {
-							deleteItem(id: $id)
-						}
-					`,
-					variables:{
-						id: item.id
-					}
-				}
-			}).then(() => {
-				this.items[item.type].splice(this.items[item.type].indexOf(item),1);
-				this.generateDomains();
-			});
-		},
-
-		getItems(type){
-			return	axios({
-				url:"http://localhost:4000",
-				method: "post",
-				data:{
-					query:`
-						query ($type: String) {
-							items: items(type: $type) {
-								id
-								type
-								description
-							}
-						}
-					`,
-					variables:{
-						type
-					}
-				}
-			})
-				.then(response => {
-					const query = response.data;
-					this.items.[type] = query.data.items;
-				});
-		},
-		generateDomains(){
-			axios({
-				url:"http://localhost:4000",
-				method: "post",
-				data:{
-					query:
-						`
-							mutation	{
-									domains: generateDomains{
-										name
-										checkout
-										available
-									}
-							}
-						`
-				}
-			}).then((response) => {
-				const query = response.data;
-				this.domains = query.data.domains;
-			});
-		},
+		...mapActions(["addItem", "deleteItem", "getItems", "generateDomains"]),
 		opendDomain(domain){
 			this.$router.push({
 				path: `/domains/${domain.name}`
@@ -163,14 +66,8 @@ export default {
 		},
 		
 	},
-	created(){
-		Promise.all([
-			this.getItems("prefix"),
-			this.getItems("sufix")
-		]).then(()=>{
-			this.generateDomains();
-		});
-	
+	computed:{
+		...mapState(["items","domains"])
 	}
 };
 </script>
